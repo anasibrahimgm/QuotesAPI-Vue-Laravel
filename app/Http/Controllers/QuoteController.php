615eb48bc15ea5824b0a5d3f3b39dbc6dafb623d
anasibrahimgm <anasibrahimgm@gmail.com>
2017-07-12 14:51:20 +0200

    02. Backend

---
+++ app/Http/Controllers/QuoteController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Quote;
+
+class QuoteController extends Controller
+{
+    public function postQuote (Request $request)
+    {
+      $quote = new Quote();
+      $quote->content = $request->input('content');
+      $quote->save();
+
+      return response()->json(['quote' => $quote], 201);
+    }
+
+    public function getQuote ()
+    {
+      $quotes = Quote::all();
+      $response = [
+        'quotes' => $quotes
+      ];
+
+      return response()->json($response, 201);
+    }
+
+    public function putQuote (Request $request, $id)
+    {
+      $quote = Quote::find($id);
+      if (!$quote) {
+        return response()->json(['message' => 'Quote Not Found'], 404);
+      }
+      $quote->content = $request->content;
+      $quote->save();
+
+      return response()->json(['quote', $quote], 200);
+    }
+
+    public function deleteQuote ($id)
+    {
+      $quote = Quote::find($id);
+      $quote->delete();
+
+      return response()->json(['message' => 'Quote Deleted'], 200);
+    }
+}
