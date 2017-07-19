c693a0fe9c4b3af6fdc311583ed3983fe52c15f1
anasibrahimgm <anasibrahimgm@gmail.com>
2017-07-19 18:28:19 +0200

    03. Vue setup and newQuotes.vue

---
+++ vue-laravel/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <new-quotes></new-quotes>
+  </div>
+</template>
+
+<script>
+import newQuotes from './components/newQuotes.vue';
+
+export default {
+  components: {
+    'new-quotes': newQuotes,
+  },
+  name: 'app',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-laravel/src/components/newQuotes.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <form @submit.prevent="submitted">
+      <div class="form-group">
+        <label for="content">Content</label>
+        <input
+              type="text"
+              id="content"
+              class="form-control"
+              v-model="quoteContent"
+        />
+      </div>
+      <button class="btn btn-primary" type="submit">Submit</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      quoteContent: '',
+    }
+  },
+
+  methods: {
+    submitted() {
+      axios.post("http://vue.app/api/quote", {
+        content: this.quoteContent
+      })
+      .then(
+        (response) => console.log(response)
+      ).catch(
+        (error) => console.log(error)
+      );
+    },
+  },
+}
+</script>
+
+<style scoped>
+</style>
